ee1ac165c0dd8361bbd1d90abf02272286ca8f71
Victor Manuel Ceballos Cruz <vimaceballoscr@ittepic.edu.mx>
2019-03-31 16:36:00 -0700

    Feat: add jwt auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssistantModule } from './assistant/assistant.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AssistantModule],
+  imports: [AssistantModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
